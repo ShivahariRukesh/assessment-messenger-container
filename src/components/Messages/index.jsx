1f50ac93bc95a58f4ac5579c50894a2a0dc49988
ShivahariRukesh <478gnujnoej@gmail.com>
2024-10-25 10:36:53 +0545

    some changes to scrollbar

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -6,7 +6,6 @@ import { List, Spin } from "antd";
 import "./styles.css";
 import { FileOutlined, GifOutlined, PlusCircleFilled } from "@ant-design/icons";
 import { getInitials } from "./helper";
-
 const Messages = () => {
   const [isFetching, setIsFetching] = useState(false);
   const dispatch = useDispatch();
@@ -14,16 +13,16 @@ const Messages = () => {
 
   useEffect(() => {
     dispatch(fetchUsers(page));
-  }, [dispatch, page]);
+  }, []);
 
   const loadMoreUsers = useCallback(() => {
     if (!hasMore || isFetching) return;
+
     setIsFetching(true);
     setTimeout(() => {
       dispatch(incrementPage());
-      dispatch(fetchUsers(page + 1)).then(() => {
-        setIsFetching(false);
-      });
+      dispatch(fetchUsers(page + 1));
+      setIsFetching(false);
     }, 500);
   }, [dispatch, page, hasMore, isFetching]);
 
@@ -57,19 +56,13 @@ const Messages = () => {
             >
               {getInitials(user.name)}
             </div>
+
             <span style={{ marginLeft: 10, marginTop: 8 }}>{user.name}</span>
           </div>
         </div>
       </List.Item>
     );
   };
-
-  useEffect(() => {
-    const scrollableDiv = document.getElementById("scrollableDiv");
-    if (scrollableDiv) {
-      scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
-    }
-  }, [users]);
 
   return (
     <div className="message-container">
@@ -117,7 +110,9 @@ const Messages = () => {
           className="message-input"
           placeholder="Just a demo..."
         />
-        <button className="send-btn">Send</button>
+        <button className="send-btn" onClick>
+          Send
+        </button>
       </div>
     </div>
   );
